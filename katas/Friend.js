e847b9d0df94f6f6a8e4273690e7e2e60c80760c
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2022-04-11 15:43:01 +0200

    adding Friend.js

--- katas/Friend.js
+++ katas/Friend.js
@@ -1,3 +1,3 @@
 export default function friend(friends){
-    return friends.filter(function (friend){return friend.length == 4})
+    return friends.filter(function (friend){return friend.length === 4})
 }
